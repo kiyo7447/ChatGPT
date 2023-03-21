74cfff0bf920516030e5d67e4ffef33e180c6f1b
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-03-21 09:40:17 +0900

    クレジット処理を追加

---
+++ CreditCardBrandDetector/CreditCardBrandDetector.py
@@ -0,0 +1,55 @@
+﻿import re
+
+def detect_credit_card_brand(card_number):
+
+    # スペースとハイフン（全角も含む）を取り除く
+    card_number = re.sub(r"[\s-]|[\u3000]|[－]", "", card_number)
+
+    # Visa
+    if re.match(r"^4[0-9]{12}(?:[0-9]{3})?$", card_number):
+        return "Visa"
+
+    # MasterCard
+    if re.match(r"^5[1-5][0-9]{14}$", card_number):
+        return "MasterCard"
+
+    # American Express
+    if re.match(r"^3[47][0-9]{13}$", card_number):
+        return "American Express"
+
+    # Diners Club
+    if re.match(r"^3(?:0[0-5]|[68][0-9])[0-9]{11}$", card_number):
+        return "Diners Club"
+
+    # Discover
+    if re.match(r"^6(?:011|5[0-9]{2})[0-9]{12}$", card_number):
+        return "Discover"
+
+    # JCB
+    if re.match(r"^(?:2131|1800|35\d{3})\d{11}$", card_number):
+        return "JCB"
+
+    # Unknown brand
+    return "Unknown"
+
+if __name__ == "__main__":
+    # Sample card number
+    card_number = "378282246310005"
+
+    # Detect credit card brand
+    brand = detect_credit_card_brand(card_number)
+
+    # Print result
+    print("Credit Card Brand:", brand)
+
+    while True:
+        # ユーザからの入力を受け付ける
+        card_number = input("Enter a credit card number (or press enter to quit): ")
+
+        # 入力が空の場合、終了
+        if not card_number:
+            break
+
+        # クレジットカードブランドの判定
+        brand = detect_credit_card_brand(card_number)
+        print("Credit Card Brand:", brand)
---
+++ CreditCardBrandDetector/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// サンプルのカード番号
+string cardNumber = "378282246310005";
+
+// クレジットカードブランドを判定
+string brand = DetectCreditCardBrand(cardNumber);
+
+// 結果を表示
+Console.WriteLine("Credit Card Brand: " + brand);
+
+while (true)
+{
+	// ユーザからの入力を受け付ける
+	Console.Write("Enter a credit card number (or press enter to quit): ");
+	cardNumber = Console.ReadLine();
+
+	// 入力が空の場合、終了
+	if (string.IsNullOrEmpty(cardNumber))
+		break;
+
+	// クレジットカードブランドの判定
+	brand = DetectCreditCardBrand(cardNumber);
+	Console.WriteLine("Credit Card Brand: " + brand);
+}
+
+
+// クレジットカード番号からブランドを判定するメソッド
+static string DetectCreditCardBrand(string cardNumber)
+{
+	// スペースとハイフン（全角も含む）を取り除く
+	cardNumber = Regex.Replace(cardNumber, @"[\s-]|[\u3000]|[－]", "");
+
+	// Visa
+	if (Regex.IsMatch(cardNumber, @"^4[0-9]{12}(?:[0-9]{3})?$"))
+		return "Visa";
+
+	// MasterCard
+	if (Regex.IsMatch(cardNumber, @"^5[1-5][0-9]{14}$"))
+		return "MasterCard";
+
+	// American Express
+	if (Regex.IsMatch(cardNumber, @"^3[47][0-9]{13}$"))
+		return "American Express";
+
+	// Diners Club
+	if (Regex.IsMatch(cardNumber, @"^3(?:0[0-5]|[68][0-9])[0-9]{11}$"))
+		return "Diners Club";
+
+	// Discover
+	if (Regex.IsMatch(cardNumber, @"^6(?:011|5[0-9]{2})[0-9]{12}$"))
+		return "Discover";
+
+	// JCB
+	if (Regex.IsMatch(cardNumber, @"^(?:2131|1800|35\d{3})\d{11}$"))
+		return "JCB";
+
+	// その他のブランド
+	return "Unknown";
+}
