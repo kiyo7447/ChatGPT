cdcc273245c3c9ebe577ff28af8af8e99cc9f96c
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-06-19 20:56:27 +0900

    Notification Hubを追加

---
+++ AzureNotificationHub/AzureNotificationHubConsoleApp/AzureNotificationHubConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AzureNotificationHubConsoleApp.Services;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional:false)
+    .AddEnvironmentVariables();
+
+IConfiguration configuration = builder.Build();
+
+string? notificationHubConnectionString = configuration["NotificationHubConnectionString"];
+string? notificationHubName = configuration["NotificationHubName"];
+
+
+//NotificationServiceを実体化して使用します。
+var notificationService = new NotificationService(notificationHubConnectionString, notificationHubName);
+notificationService.SendNotificationAsync("Hello, World!", new List<string>() { "uid-222", "tag2" }).Wait();
+
---
+++ AzureNotificationHub/AzureNotificationHubConsoleApp/AzureNotificationHubConsoleApp/Services/NotificationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
+using System.Collections.Generic;
+
+namespace AzureNotificationHubConsoleApp.Services
+{
+    public class NotificationService
+    {
+        private NotificationHubClient _hub;
+
+        public NotificationService(string? notificationHubConnectionString=null, string? notificationHubName=null)
+        {
+            _hub = NotificationHubClient.CreateClientFromConnectionString(notificationHubConnectionString, notificationHubName);
+        }
+
+        public async Task SendNotificationAsync(string message, List<string> tags)
+        {
+            // Android notification payload
+            var androidPayload = "{ \"data\" : {\"message\":\"" + message + "\"}}";
+
+            // iOS notification payload
+            var applePayload = "{\"aps\":{\"alert\":\"" + message + "\"}}";
+
+            // Windows (UWP) notification payload
+            var windowsPayload = @"<toast><visual><binding template=""ToastText01""><text id=""1"">" +
+                                    message + "</text></binding></visual></toast>";
+
+            // Send the message to the specified tags
+            await _hub.SendFcmNativeNotificationAsync(androidPayload, tags);
+            await _hub.SendAppleNativeNotificationAsync(applePayload, tags);
+            await _hub.SendWindowsNativeNotificationAsync(windowsPayload, tags);
+        }
+    }
+}
